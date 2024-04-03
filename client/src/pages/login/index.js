5eefdb2df75e56a557f41119817bb0c4278e20a9
ALISSON LAVORATTI DA CONCEIÇÃO <109707293+AlissonLavoratti@users.noreply.github.com>
2024-04-03 01:01:22 -0300

    Tela de login criada
    Tela de login criada

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './style.css';
+import logoImage from '../../assets/logo.svg'
+import padlock from '../../assets/padlock.png'
 
 export default function Login(){
     // Este é um componente do React
     // Se for só uma linha o conteúdo do return  não precisa de "()".
     // Se for mais de uma linha o conteúdo do return então precisa de "()"
-    return (
-        <div>
-            <h1>Wellcome</h1>        
-            <h2>Well</h2>
-        </div>
+    return (  
+            <div className="login-container">
+                <section className="form">
+                    <img src={logoImage} alt="Erudio Logo"/>
+                    <form action="">
+                        <h1>Acess your account</h1>
+                        <input type="text" placeholder='Username'/>
+                        <input type="password" placeholder='Password'/>
+                        <button className='button' type='submit'>Login</button>
+                    </form>
+                </section>
+                <img src={padlock} alt="Login"/>
+            </div>
     )
 }
 
